85dee18e2e7b149746560c80bcf71a8a3bfb6729
Akram Amin <akraminum379@outlook.com>
2022-01-12 19:46:54 +0200

    .

---
+++ TrickingLibrary.API/TrickingLibrary.API/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrickingLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "HELLO WORLD";
+        }
+    }
+}
